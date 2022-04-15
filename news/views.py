d2a83ab10bc800281f4edfc1ece3551cb2c7eedc
tnazarbekov <tnazarbekov@bk.ru>
2022-04-15 10:59:33 +0600

    news add,remove,detail,create

---
+++ news/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import Articles
+from .forms import ArticlesForm
+from django.views.generic import DetailView, UpdateView, DeleteView
+
+
+# Create your views here.
+def news_home(request):
+    news = Articles.objects.all()
+    return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+    template_name = 'news/create.html'
+    form_class = ArticlesForm
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsDeleteView(DeleteView):
+    model = Articles
+    template_name = 'news/news-delete.html'
+    success_url = '/news/'
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Неверная форма'
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'news/create.html', data)
